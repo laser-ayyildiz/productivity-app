68b703d38ea6e4b45fe9ae39ddcee974a8c51b40
ozgebuyukasik <ozge.bykaskk@gmail.com>
2022-06-05 23:19:20 +0300

    add mux router

--- habit-service/main.go
+++ habit-service/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"habit-service/router"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	router := router.Router()
+	fmt.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
