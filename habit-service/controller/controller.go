a5b9758d6e4a4be61adb05b92caec2772e65e631
ozgebuyukasik <ozge.bykaskk@gmail.com>
2022-06-06 21:25:40 +0300

    add controller functions

---
+++ habit-service/controller/controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"log"
+	"net/http"
+)
+
+func setHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
+func InsertHabit(responseWriter http.ResponseWriter, req *http.Request) {
+	setHeaders(responseWriter)
+	responseWriter.Header().Set("Access-Control-Allow-Methods", "POST")
+
+	log.Println("Inserting habit")
+}
+func UpdateHabit(responseWriter http.ResponseWriter, req *http.Request) {
+	setHeaders(responseWriter)
+	responseWriter.Header().Set("Access-Control-Allow-Methods", "POST")
+
+	log.Println("Updating habit")
+}
+func DeleteHabit(responseWriter http.ResponseWriter, req *http.Request) {
+	setHeaders(responseWriter)
+	responseWriter.Header().Set("Access-Control-Allow-Methods", "POST")
+
+	log.Println("Deleting habit")
+}
+
