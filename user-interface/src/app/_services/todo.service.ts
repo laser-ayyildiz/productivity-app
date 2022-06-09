c83fe02d8a15171945c8748be04471b6f97720c7
LASER AYYILDIZ <laserayyildiz@gmail.com>
2022-06-09 20:31:23 +0300

    Merge branch 'user-interface' into dev

--- user-interface/src/app/_services/todo.service.ts
+++ user-interface/src/app/_services/todo.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TokenStorageService } from './token-storage.service';
 
-const TODO_API = 'http://localhost:8080/todo-service/task';
+const TASK_API = 'http://localhost:8080/todo-service/task';
 const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,14 @@ export class TodoService {
   constructor(private http: HttpClient, private tokenStorage: TokenStorageService) { }
 
   getAll(pageSize: number = 10, pageNumber: number = 0): Observable<any> {
-    return this.http.get(`${TODO_API}?pageNumber=${pageNumber}&pageSize=${pageSize}`, httpOptions);
+    return this.http.get(`${TASK_API}?pageNumber=${pageNumber}&pageSize=${pageSize}`, httpOptions);
+  }
+
+  create(category: string, title: string, habitId: number, deadline: Date,
+    description: string, priority: Number, color: string): Observable<any> {
+    console.log("servsteyim")
+    return this.http.post(TASK_API, {
+      category, title, habitId, deadline, description, priority, color
+    }, httpOptions);
   }
 }
--- user-interface/src/app/todo/todo.component.ts
+++ user-interface/src/app/todo/todo.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { TodoService } from '../_services/todo.service';
 
-
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -11,8 +10,16 @@ import { TodoService } from '../_services/todo.service';
 })
 export class TodoComponent implements OnInit {
   errorMessage: string = "";
-
   todos: any = {}
+  newTodo: any = {
+    category: "",
+    title: "",
+    habitId: 0,
+    deadline: null,
+    description: "",
+    priority: 0,
+    color: ""
+  };
 
   constructor(private route: ActivatedRoute, private todoService: TodoService, private toastr: ToastrService) { }
 
@@ -23,8 +30,23 @@ export class TodoComponent implements OnInit {
       },
       error: err => {
         this.errorMessage = err.error.message;
-        this.toastr.error('Control your credentials!');
+        this.toastr.error('We have a problem Houston!');
       }
-    });;
+    });
+  }
+
+  onSubmit(): void {
+    const { category, title, habitId, deadline, description, priority, color } = this.newTodo;
+    console.log("componentteyim")
+    this.todoService.create(category, title, habitId, deadline,
+      description, priority, color).subscribe({
+        next: data => {
+          console.log(data);
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.toastr.error('Control your inputs!');
+        }
+      });
   }
 }
